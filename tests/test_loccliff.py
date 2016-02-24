92cda9c3c6cef41c71d0ac8e008af55ee9939c96
Pete Shadbolt <pete.shadbolt@gmail.com>
2016-02-24 13:40:29 +0000

    Add branch

--- tests/test_loccliff.py
+++
@@ -1,6 +0,0 @@
-from nose import with_setup
-import loccliff
-
-def test_loccliff():
-    pass
-
--- tests/test_rightphase.py
+++
@@ -1,6 +0,0 @@
-from nose import with_setup
-from rightphase import RightPhase
-
-def test_rightphase():
-    assert RightPhase(4) == 4
-
