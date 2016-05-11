37404bc535d2c0283802457ef2a8d44d5408cd96
Pete Shadbolt <pete.shadbolt@gmail.com>
2016-05-11 13:19:53 +0100

    Remove QTIP dependency. Not needed

--- tests/test_qi_vs_qtip.py
+++
@@ -1,9 +0,0 @@
-from abp import qi
-from qutip import *
-import numpy as np
-
-def test_1():
-    q = QubitCircuit(4)
-    q.add_gate("CPHASE", 1, 2, arg_value=np.pi)
-    #print gate_sequence_product(q.propagators())
-
