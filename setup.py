6763052d4f398bbc929c4f603d57492e3930d82e
Pete Shadbolt <pete.shadbolt@gmail.com>
2016-07-23 17:51:51 +0100

    Trying to get `abserver` to install automatically

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
 from setuptools import setup, find_packages
+from glob import glob
+from os import path
+
+APPNAME = "abp"
+STATIC = glob("static/*.*")+glob("static/img/*.*")+glob("static/scripts/*.*")
+appdata = path.expanduser(path.join("~", "." + APPNAME))
+print appdata
 
 setup(
     name = "abp",
@@ -9,5 +16,7 @@ setup(
     description = "Port of C++ due to Simon Anders and Hans J Briegel",
     keywords = "quantum",
     setup_requires = ["numpy"],
+    scripts = ["bin/abpserver"],
     install_requires = ["numpy", "networkx", "tqdm", "websocket-client", "websocket-server"],
+    data_files = [("static", STATIC)]
 )
