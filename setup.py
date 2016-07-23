3e2823d7b54e041eea8b4c7968fbc576d36053d1
Pete Shadbolt <pete.shadbolt@gmail.com>
2016-07-23 18:13:11 +0100

    Remove old debug code

--- setup.py
+++ setup.py
@@ -9,10 +9,12 @@ print appdata
 
 setup(
     name = "abp",
-    version = "0.3",
+    version = "0.4",
     packages = find_packages(),
     test_suite = "tests",
     author = "Pete Shadbolt",
+    author_email = "hello@peteshadbolt.co.uk",
+    url = "https://github.com/peteshadbolt/abp",
     description = "Port of C++ due to Simon Anders and Hans J Briegel",
     keywords = "quantum",
     setup_requires = ["numpy"],
