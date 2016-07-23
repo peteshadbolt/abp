85d6c262d9bdbaa76a68c522b5169a5672a85ebb
Pete Shadbolt <pete.shadbolt@gmail.com>
2016-07-23 18:25:03 +0100

    PIP seems to be kinda working now

--- setup.py
+++ setup.py
@@ -2,14 +2,11 @@ from setuptools import setup, find_packages
 from glob import glob
 from os import path
 
-APPNAME = "abp"
 STATIC = glob("static/*.*")+glob("static/img/*.*")+glob("static/scripts/*.*")
-appdata = path.expanduser(path.join("~", "." + APPNAME))
-print appdata
 
 setup(
     name = "abp",
-    version = "0.4",
+    version = "0.4.1",
     packages = find_packages(),
     test_suite = "tests",
     author = "Pete Shadbolt",
@@ -20,5 +17,5 @@ setup(
     setup_requires = ["numpy"],
     scripts = ["bin/abpserver"],
     install_requires = ["numpy", "networkx", "tqdm", "websocket-client", "websocket-server"],
-    data_files = [("static", STATIC)]
+    package_data = {"abp.static": STATIC}
 )
