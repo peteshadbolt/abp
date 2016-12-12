fbbe57ae3132a211f46777050732b255bb16128b
Pete Shadbolt <hello@peteshadbolt.co.uk>
2016-12-12 08:07:05 -0800

    Bump version: 0.4.23 → 0.4.24

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ STATIC = glob("abp/static/*.*")+glob("abp/static/img/*.*")+glob("abp/static/scri
 
 setup(
     name = "abp",
-    version = "0.4.23",
+    version = "0.4.24",
     packages = ["abp", "abp.static"],
     test_suite = "tests",
     author = "Pete Shadbolt",
