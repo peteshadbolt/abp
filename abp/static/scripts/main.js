299efef9070d125736cc4b198e4546160ae7d878
Pete Shadbolt <hello@peteshadbolt.co.uk>
2016-12-14 08:02:14 -0800

    Print version from JS.

--- abp/static/scripts/main.js
+++ abp/static/scripts/main.js
@@ -1,10 +1,10 @@
+console.log("abp v0.4.24");
+
 window.onload = function() {
     graph.prepare();
     materials.prepare();
     gui.prepare();
     mouse.prepare();
     editor.prepare();
-    gui.scene.children[0].renderOrder = -1000;
-    console.log(gui.scene.children[0].renderOrder = -1000);
     gui.loop();
 };
