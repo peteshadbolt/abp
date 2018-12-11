c46f47ac884076837abb3d0d3fee5de1b97a1464
Sam Morley-Short <sam.morleyshort@psiquantum.com>
2018-12-11 08:37:53 -0800

    Added updated Clifford table

--- abp/clifford.py
+++ abp/clifford.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 """
-This module handles operations on the Clifford group. It makes extensive use of the lookup tables in ``abp.tables``. 
+This module handles operations on the Clifford group. It makes extensive use of the lookup tables in ``abp.tables``.
 The code to generate those tables is included in this distribution as ``abp/build_tables.py``
 This package emumerates and labels the single-qubit Clifford group, and provides methods for matrix multiplication and conjugation.
 It also includes the look-up table for the CZ gate.
@@ -9,34 +9,35 @@ It also includes the look-up table for the CZ gate.
 There are 24 members of the single-qubit Clifford group. You can refer to some of them by multiple names.
 The complete set of aliases for single-qubit Cliffords is as follows:
 
-    ======= =========================
-    Index   Aliases
-    ======= =========================
-    0       ``IA, identity, identity_h``
-    1       ``XA, px, px_h``
-    2       ``YA, py, py_h``
-    3       ``ZA, pz, pz_h``
-    4       ``IB``
-    5       ``XB, sqz, msqz_h, phase_h``
-    6       ``YB, msqz, sqz_h, phase``
-    7       ``ZB``
-    8       ``IC``
-    9       ``XC, msqy, sqy_h``
-    10      ``YC, hadamard, hadamard_h``
-    11      ``ZC, sqy, msqy_h``
-    12      ``ID``
-    13      ``XD``
-    14      ``YD, sqx, msqx_h``
-    15      ``ZD, msqx, sqx_h``
-    16      ``IE``
-    17      ``XE``
-    18      ``YE``
-    19      ``ZE``
-    20      ``IF``
-    21      ``XF``
-    22      ``YF``
-    23      ``ZF``
-    ======= =========================
+    === ====================== ===================== ====================== ======================= =========
+    VOP Clifford               :math:`U^\\dagger XU`  :math:`U^\\dagger YU`   :math:`U^\\dagger ZU`     ABP Alias
+                         
+    === ====================== ===================== ====================== ======================= =========
+    0   :math:`I`                   :math:`X`             :math:`Y`             :math:`Z`              ``IA, identity, identity_h``
+    1   :math:`X`                   :math:`X`            :math:`-Y`            :math:`-Z`              ``XA, px, px_h``
+    2   :math:`Y`                  :math:`-X`             :math:`Y`            :math:`-Z`              ``YA, py, py_h``
+    3   :math:`Z`                  :math:`-X`            :math:`-Y`             :math:`Z`              ``ZA, pz, pz_h``
+    4   :math:`XS`                 :math:`-Y`            :math:`-X`            :math:`-Z`              ``IB``
+    5   :math:`S^\\dagger`         :math:`-Y`             :math:`X`             :math:`Z`              ``XB, sqz, msqz_h, phase_h``
+    6   :math:`S`                   :math:`Y`            :math:`-X`             :math:`Z`              ``YB, msqz, sqz_h, phase``
+    7   :math:`XS^\\dagger`         :math:`Y`             :math:`X`            :math:`-Z`              ``ZB``
+    8   :math:`YH`                 :math:`-Z`            :math:`-Y`            :math:`-X`              ``IC``
+    9   :math:`XH`                 :math:`-Z`             :math:`Y`             :math:`X`              ``XC, msqy, sqy_h``
+    10  :math:`H`                   :math:`Z`            :math:`-Y`             :math:`X`              ``YC, hadamard, hadamard_h``
+    11  :math:`ZH`                  :math:`Z`             :math:`Y`            :math:`-X`              ``ZC, sqy, msqy_h``
+    12  :math:`S^\\dagger HS`      :math:`-X`            :math:`-Z`            :math:`-Y`              ``ID``
+    13  :math:`SHS^\\dagger`       :math:`-X`             :math:`Z`             :math:`Y`              ``XD``
+    14  :math:`SHS`                 :math:`X`            :math:`-Z`             :math:`Y`              ``YD, sqx, msqx_h``
+    15  :math:`HSH`                 :math:`X`             :math:`Z`            :math:`-Y`              ``ZD, msqx, sqx_h``
+    16  :math:`HS^\\dagger`         :math:`Y`             :math:`Z`             :math:`X`              ``IE``
+    17  :math:`YHS^\\dagger`        :math:`Y`            :math:`-Z`            :math:`-X`              ``XE``
+    18  :math:`YHS`                :math:`-Y`             :math:`Z`            :math:`-X`              ``YE``
+    19  :math:`HS`                 :math:`-Y`            :math:`-Z`             :math:`X`              ``ZE``
+    20  :math:`SH`                  :math:`Z`             :math:`X`             :math:`Y`              ``IF``
+    21  :math:`S^\\dagger H`        :math:`Z`            :math:`-X`            :math:`-Y`              ``XF``
+    22  :math:`XSH`                :math:`-Z`             :math:`X`            :math:`-Y`              ``YF``
+    23  :math:`XS^\\dagger H`      :math:`-Z`            :math:`-X`             :math:`Y`              ``ZF``
+    === ====================== ===================== ====================== ======================= =========
 
 """
 
